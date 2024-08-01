6613540ddc9a6793ae079f40a55180d3cea642b9
widawek <kolczywiewicz@gmail.com>
2024-08-01 20:44:13 +0200

    2Democracy working version 1.10

--- parameters.py
+++ parameters.py
@@ -3,14 +3,12 @@ from model_generator import min_factor, max_factor
 
 
 intervals = ['M5', 'M6', 'M10', 'M12']
-symbols = ['EURUSD', 'USDCAD', 'GBPUSD']
-# intervals = ['M5', 'M6', 'M10', 'M12']
-#leverage = 30
+symbols = ['EURUSD', 'GBPUSD']
 leverage = 30
 delete_old_models = True
 positions_number = 2
-max_reducer = 15
-min_reducer = 6
+max_reducer = 12
+min_reducer = 5
 volatility_divider = [reduce_values(intervals, min_factor, max_factor), max_reducer, min_reducer]  
 print(volatility_divider)
 # absolute, weighted_democracy, ranked_democracy, just_democracy, invertedrank_democracy
