4384640a684279b9df51bd6fe49c303f7501a438
widawek <kolczywiewicz@gmail.com>
2024-11-16 16:33:51 +0100

    Rename simple_random_bot to main in play.py file. Alpha 0.0020

--- play.py
+++ play.py
@@ -2,18 +2,16 @@ import subprocess
 from app.functions import timer, want_to_delete_old_models
 from app.model_generator import generate_my_models
 from config.parameters import *
-import sys
-sys.path.append("")
 
-generate_my_models(symbols, intervals, leverage, want_to_delete_old_models())
+#generate_my_models(symbols, intervals, leverage, want_to_delete_old_models())
 timer(7)
 
-# from app.simple_random_bot import Bot; 
+# from app.main import Bot; 
 # bot = Bot('BTCUSD')
 # bot.report()
 
 for symbol in symbols:
-    command = 'start cmd /k python -c "from app.simple_random_bot import Bot; bot = Bot(\'{}\'); bot.report()"'.format(symbol)
+    command = 'start cmd /k python -c "from app.main import Bot; bot = Bot(\'{}\'); bot.report()"'.format(symbol)
     subprocess.Popen(command, shell=True)
 
 input()
