46ffcd66ab1392e0523bd1b7576f9fe310dab957
widawek <108025750+widawek@users.noreply.github.com>
2024-12-19 11:10:46 +0100

    Update parameters.py

--- config/parameters.py
+++ config/parameters.py
@@ -58,7 +58,7 @@ start_trigger: str                      = 'moving_averages'
 global_tracker_multiplier: float        = 1.1
 profit_decrease_barrier: float          = 0.93
 profit_increase_barrier: float          = 1.6
-respect_overnight: bool                 = True
+respect_overnight: bool                 = False
 
 def reverse_(symbol):
     if symbol in []:
