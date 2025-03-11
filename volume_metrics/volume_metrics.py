441d7cc08e0967e802fdce028de35b364cb61eb0
widawek <kolczywiewicz@gmail.com>
2025-03-11 09:23:33 +0100

    Alpha 0.0072

--- volume_metrics/volume_metrics.py
+++ volume_metrics/volume_metrics.py
@@ -195,7 +195,7 @@ class SymbolsByProfile:
         for i in tqdm(self.all_combinations):
             df = self.all_returns.copy()
             df['return'] = np.sum(df[i], axis=1)
-            results.append((i, final_metric_(df))) # change metric for last best metric
+            results.append((i, final_metric_(df, penalty=False))) # change metric for last best metric
 
         self.results_df_raw = pd.DataFrame(results, columns=['combination', 'sharpe_omega']).sort_values(by='sharpe_omega', ascending=False)
         self.results_df = self.results_df_raw[self.results_df_raw['sharpe_omega'] > 0.8*self.results_df_raw['sharpe_omega'].max()]
